008be484291d574a0986a526f22608f330efd744
git4nm <37463465+git4nm@users.noreply.github.com>
2018-11-10 09:11:23 +1100

    Delete Command.bat

--- Command.bat
+++
@@ -1,9 +0,0 @@
-@Echo off
-
-"Navigating to bin folder"
-cmd.exe /c cd C:\AirWallex_CodeChallenge\soapui\bin
-
-"Executing testcases using testRunner"
-testrunner.bat -j -r -fC:\AirWallex_CodeChallenge\Results -PURL="http://preview.airwallex.com:30001" C:\AirWallex_CodeChallenge\App\AirWallexTest-soapui-project.xml
-
-Exit
